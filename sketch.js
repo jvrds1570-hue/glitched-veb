bceb49e0ea95a6985464ac3e273cdc8b9fe499fa
jvrds1570-hue <jvrds15.70@gmail.com>
2026-03-18 02:03:28 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+let capture;
+let img;
+let angle = 0;
+
+
+function preload() {
+  font = loadFont("nimbusmono-bold.otf");}
+
+function setup() {
+  createCanvas(720, 400, WEBGL);
+  
+  noCursor();
+
+  textFont(font);
+  textSize(20);
+  textWrap(CHAR);
+  frameRate(9)
+  
+  capture = createCapture(VIDEO);
+  capture.size(360, 200);
+  capture.hide();
+  
+  img = createGraphics(360, 200);
+}
+
+function draw() {
+ 
+  background(0);
+  
+  angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);
+  
+  img.image(capture, 0, 0);
+  img.filter(THRESHOLD);
+  
+  push();
+
+   rotateY(angle);
+
+  if (cos(angle) < 0) {
+    texture(img);
+      filter(INVERT);
+    img.filter(INVERT);
+  } else {
+    texture(img);
+  }
+  
+  noStroke();
+  plane(360, 200);
+
+  pop();
+  
+  {
+push();
+
+translate(0, 0, -200 + angle * 100);
+
+noStroke();
+fill(255);
+
+let noiseScale = 0.02;
+let t = frameCount * 0.05;
+
+
+for (let i = 0; i < 70; i++) {
+  let y = random(-height*1.5, height*1.5);
+  let h = random(1, 5);
+  let x = random(-width*1.5, width*1.5);
+
+  rect(x, y, x, h);
+}
+pop();
+  }
+  
+  fill(0);
+  translate(0, 0, -300 + angle * 20);
+  text('let capture;let img;let angle = 0;let font1;let font2;function preload() {font = loadFont("nimbusmono-bold.otf");}function setup() {createCanvas(720, 400, WEBGL);textFont(font);textSize(12);frameRate(12)capture = createCapture(VIDEO);capture.size(360, 200);capture.hide();img = createGraphics(360, 200);}function draw() {background(0);angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);img.image(capture, 0, 0)img.filter(THRESHOLD);push();rotateY(angle);if (cos(angle) < 0) {texture(img);filter(INVERT);img.filter(INVERT);} else {texture(img);}noStroke();plane(360, 200);pop();}function mousePressed() {if (isLooping()) {noLoop();} else {loop();}}let capture;let img;let angle = 0;let font1;let font2;function preload() {font = loadFont("nimbusmono-bold.otf");}function setup() {createCanvas(720, 400, WEBGL);textFont(font);textSize(12);frameRate(12)capture = createCapture(VIDEO);capture.size(360, 200);capture.hide();img = createGraphics(360, 200);}function draw() {background(0);angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);img.image(capture, 0, 0)img.filter(THRESHOLD);push();rotateY(angle);if (cos(angle) < 0) {texture(img);filter(INVERT);img.filter(INVERT);} else {texture(img);}noStroke();plane(360, 200);pop();}function mousePressed() {if (isLooping()) {noLoop();} else {loop();}}let capture;let img;let angle = 0;let font1;let font2;function preload() {font = loadFont("nimbusmono-bold.otf");}function setup() {createCanvas(720, 400, WEBGL);textFont(font);textSize(12);frameRate(12)capture = createCapture(VIDEO);capture.size(360, 200);capture.hide();img = createGraphics(360, 200);}function draw() {background(0);angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);img.image(capture, 0, 0)img.filter(THRESHOLD);push();rotateY(angle);if (cos(angle) < 0) {texture(img);filter(INVERT);img.filter(INVERT);} else {texture(img);}noStroke();plane(360, 200);pop();}function mousePressed() {if (isLooping()) {noLoop();} else {loop();}}let capture;let img;let angle = 0;let font1;let font2;function preload() {font = loadFont("nimbusmono-bold.otf");}function setup() {createCanvas(720, 400, WEBGL);textFont(font);textSize(12);frameRate(12)capture = createCapture(VIDEO);capture.size(360, 200);capture.hide();img = createGraphics(360, 200);}function draw() {background(0);angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);img.image(capture, 0, 0)img.filter(THRESHOLD);push();rotateY(angle);if (cos(angle) < 0) {texture(img);filter(INVERT);img.filter(INVERT);} else {texture(img);}noStroke();plane(360, 200);pop();}function mousePressed() {if (isLooping()) {noLoop();} else {loop();}}let capture;let img;let angle = 0;let font1;let font2;function preload() {font = loadFont("nimbusmono-bold.otf");}function setup() {createCanvas(720, 400, WEBGL);textFont(font);textSize(12);frameRate(12)capture = createCapture(VIDEO);capture.size(360, 200);capture.hide();img = createGraphics(360, 200);}function draw() {background(0);angle = lerp(angle, map(mouseX, 0, width, -PI, PI), 0.1);', -720, -290, width*2, height*2);
+}
+
+function mousePressed() {
+  
+  if (isLooping()) {
+    noLoop();
+  } else {
+    loop();
+  }
+  
+}
+ 
